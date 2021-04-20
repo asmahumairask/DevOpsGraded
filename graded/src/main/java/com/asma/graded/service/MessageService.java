4cf1913abaf1b0d27fa35ba1d5932801a15541b0
student <shaikasmahumaira.sk@hcl.com>
2021-04-20 07:49:11 +0000

    first commit

---
+++ graded/src/main/java/com/asma/graded/service/MessageService.java
@@ -0,0 +1,20 @@
+package com.asma.graded.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+	@Value("${name:unknown}")
+	private String name;
+
+	public String getMessage() {
+		return getMessage(name);
+	}
+
+	public String getMessage(String name) {
+		return "Hello " + name;
+	}
+
+}
---
+++ graded/src/test/java/com/asma/graded/GradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asma.graded;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
